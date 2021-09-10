2c010f8d48f8c03c69588289d8ce0f0441111b10
XiheartXihark <Xihark@gmail.com>
2021-09-10 11:15:43 -0700

    Fix Cat to actually destroy urns

--- xiharks-lbal-tweaks/patches/Cat.gd
+++ xiharks-lbal-tweaks/patches/Cat.gd
@@ -13,6 +13,7 @@ func add_conditional_effects(symbol, adjacent):
     for i in adjacent:
         symbol.add_effect_for_symbol(i, effect().if_type("goldfish").set_destroyed().animate("shake", [symbol, i]))
         symbol.add_effect_for_symbol(i, effect().if_type("goldfish").if_destroyed(true).set_target(symbol).change_value_bonus(symbol.values[0]))
+        symbol.add_effect_for_symbol(i, effect().if_type("urn").set_destroyed().animate("shake", [symbol, i]))
 
 func patch_groups(groups):
     groups.push_back("night")

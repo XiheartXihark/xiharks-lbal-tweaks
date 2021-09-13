8bd896fdeaea3d296d7d08b157ade103df972051
XiheartXihark <Xihark@gmail.com>
2021-09-13 10:53:31 -0700

    Fixing Group Patcher

--- xiharks-lbal-tweaks/patches/GroupPatcher.gd
+++ xiharks-lbal-tweaks/patches/GroupPatcher.gd
@@ -5,6 +5,7 @@ var value_index: int
 func init(modloader: Reference, params):
     self.modloader = modloader
     self.id = params.id
+    self.group = params.group
 
 func patch_groups(groups):
     groups.push_back(self.params.group)
